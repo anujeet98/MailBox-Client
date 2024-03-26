c62fe83725f752edcc63cac0f2efc0966f3f923f
Anujeet Swain <anujeet98@gmail.com>
2024-03-26 17:57:23 +0530

    added home page after login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 // import React from 'react';
 import Authentication from './components/Authentication/Authentication';
 import { Route, Routes } from 'react-router-dom';
+import Mailbox from './components/Mailbox/Mailbox';
 
 function App() {
   return (
     <Routes>
         <Route path='/' element={<Authentication/>} />
-        <Route path='/home' element={<Authentication/>} />
+        <Route path='/home' element={<Mailbox/>} />
     </Routes>
   );
 }
---
+++ frontend/src/components/Mailbox/Mailbox.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mailbox = () => {
+  return (
+    <div>Welcome to MailBox</div>
+  )
+}
+
+export default Mailbox
